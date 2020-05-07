1273daa873880b4b9184cf451063a902f9d3ff00
pit-ray <pit-ray@outlook.com>
2020-05-07 16:37:24 +0900

    fixed variable names

--- generator.py
+++ generator.py
@@ -32,31 +32,31 @@ class SPADEGenerator(Chain):
             self.head = define_conv(opt)(label_ch, init_shape[0], ksize=3, pad=1, initialW=he_w)
 
             #512 x 4 x 4
-            self.rf1 = SPADEResBlock(opt, ngf * 8, ngf * 8)
-            self.upf1 = define_upsampling(opt, ngf * 8)
-
-            #512 x 8 x 8
             self.r1 = SPADEResBlock(opt, ngf * 8, ngf * 8)
             self.up1 = define_upsampling(opt, ngf * 8)
 
-            #512 x 16 x 16
+            #512 x 8 x 8
             self.r2 = SPADEResBlock(opt, ngf * 8, ngf * 8)
             self.up2 = define_upsampling(opt, ngf * 8)
 
+            #512 x 16 x 16
+            self.r3 = SPADEResBlock(opt, ngf * 8, ngf * 8)
+            self.up3 = define_upsampling(opt, ngf * 8)
+
             #512 x 32 x 32
-            self.r3 = SPADEResBlock(opt, ngf * 8, ngf * 4)
-            self.up3 = define_upsampling(opt, ngf * 4)
+            self.r4 = SPADEResBlock(opt, ngf * 8, ngf * 4)
+            self.up4 = define_upsampling(opt, ngf * 4)
 
             #256 x 64 x 64
-            self.r4 = SPADEResBlock(opt, ngf * 4, ngf * 2)
-            self.up4 = define_upsampling(opt, ngf * 2)
+            self.r5 = SPADEResBlock(opt, ngf * 4, ngf * 2)
+            self.up5 = define_upsampling(opt, ngf * 2)
 
             #128 x 128 x 128
-            self.r5 = SPADEResBlock(opt, ngf * 2, ngf)
-            self.up5 = define_upsampling(opt, ngf)
+            self.r6 = SPADEResBlock(opt, ngf * 2, ngf)
+            self.up6 = define_upsampling(opt, ngf)
 
             #64 x 256 x 256
-            self.r6 = SPADEResBlock(opt, ngf, ngf // 2)
+            self.r7 = SPADEResBlock(opt, ngf, ngf // 2)
 
             #32 x 256 x 256
             self.to_img = L.Convolution2D(ngf // 2, 3, ksize=3, pad=1, initialW=xavier_w)
@@ -71,31 +71,31 @@ class SPADEGenerator(Chain):
         h = self.head_reshape(label)
         h = self.head(h)
 
-        h = self.rf1(h, w1)
-        h = self.upf1(h)
-
-        #8
         h = self.r1(h, w1)
         h = self.up1(h)
 
-        #16
+        #8
         h = self.r2(h, w1)
         h = self.up2(h)
 
-        #32
+        #16
         h = self.r3(h, w1)
         h = self.up3(h)
 
-        #64
+        #32
         h = self.r4(h, w1)
         h = self.up4(h)
 
-        #128
-        h = self.r5(h, w1, label2=w2)
+        #64
+        h = self.r5(h, w1)
         h = self.up5(h)
 
-        #256
+        #128
         h = self.r6(h, w1, label2=w2)
+        h = self.up6(h)
+
+        #256
+        h = self.r7(h, w1, label2=w2)
         h = F.leaky_relu(h)
 
         h = self.to_img(h)
